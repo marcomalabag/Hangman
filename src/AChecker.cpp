b0ca370f8ca275f2d1c8066a978b041d9cbb96f3
sinnedsynd <39647623+sinnedsynd@users.noreply.github.com>
2018-05-27 17:20:12 +0800

    merge AChecker to dev (#3)
    * commit AChecker

--- src/AChecker.cpp
+++ src/AChecker.cpp
@@ -2,4 +2,11 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
+char AChecker::checkMyLetter(char strInput)
+{
+	if(strInput == 'A' || strInput == 'a')
+		return 'a';
+	else
+		return defaultLetter;
+}
 
