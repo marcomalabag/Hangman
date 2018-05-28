860f10abf34b55a34a60b16919596f55f0b624d5
HSaliot <no>
2018-05-28 11:53:04 +0800

    TChecker wrong
    smth rong

--- src/TChecker.cpp
+++ src/TChecker.cpp
@@ -3,3 +3,11 @@
 //override the function "checkMyLetter"
 //create the function definition here
 
+char TChecker::checkMyLetter(char inputChar) 
+{	
+	if(inputChar == 'H')
+		return 'h';
+		
+	else
+		return '?';
+}
