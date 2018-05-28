292520a2a127313c262ae978cd87903e8d14f82e
ajctan <33886537+ajctan@users.noreply.github.com>
2018-05-28 11:32:31 +0800

    accidentally did V instead of W (╯°□°）╯︵ ┻━┻

--- include/WChecker.h
+++ include/WChecker.h
@@ -2,6 +2,7 @@
 
 class WChecker : public LetterChecker
 {	
+	char checkMyLetter(char inputChar);
 	//override the function "checkMyLetter"	
 	//create the function declaration here
 };
--- src/WChecker.cpp
+++ src/WChecker.cpp
@@ -1,5 +1,7 @@
 #include "WChecker.h"
 
+char WChecker::checkMyLetter(char inputChar){if(inputChar == 'W' || inputChar == 'w')return 'w';else return defaultLetter;}//override the function "checkMyLetter"//create the function definition here
+
 //override the function "checkMyLetter"
 //create the function definition here
 
