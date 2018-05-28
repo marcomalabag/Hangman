539d044fdc921a5bad5abce06476353899bbf00d
Marl Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-05-28 11:35:45 +0800

    Overrided the LetterChecker functions

--- include/SChecker.h
+++ include/SChecker.h
@@ -4,4 +4,11 @@ class SChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
 	//create the function declaration here
+	public: 
+		virtual char checkMyLetter(char inputChar);
+		SChecker();
+		
+	protected:
+		char defaultLetter;
 };
+
