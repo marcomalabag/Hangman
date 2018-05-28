60d8bfa107aa0e59883541275ab9d51679bad3ec
Cyrus Vatandoost <vatandoostcyrus@gmail.com>
2018-05-28 11:25:23 +0800

    Add YChecker
    Added YChecker class to check if `y` is part of the answer.

--- include/YChecker.h
+++ include/YChecker.h
@@ -2,6 +2,8 @@
 
 class YChecker : public LetterChecker
 {	
-	//override the function "checkMyLetter"	
+	//override the function "checkMyLetter"
 	//create the function declaration here
+	public:
+		char checkMyLetter(char inputChar);
 };
--- src/YChecker.cpp
+++ src/YChecker.cpp
@@ -2,4 +2,7 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
-
+char YChecker::checkMyLetter(char inputChar)
+{
+	return 'y';
+}
