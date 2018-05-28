337d847bc9ae1fe252ef7c89e519ad1d2542d998
jjesuscastro <joesei_castro@dlsu.edu.ph>
2018-05-28 11:22:40 +0800

    DOES NOT CALL THE FUNCTION
    still not working idk man. it doesn't call the function

--- include/EChecker.h
+++ include/EChecker.h
@@ -4,4 +4,6 @@ class EChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
 	//create the function declaration here
+public: 	
+	virtual char checkMyLetter(char inputChar);
 };
--- src/EChecker.cpp
+++ src/EChecker.cpp
@@ -1,5 +1,13 @@
 #include "EChecker.h"
 
-//override the function "checkMyLetter"
-//create the function definition here
-
+//always returns defaultLetter :P	
+char EChecker::checkMyLetter(char inputChar) 
+{
+	//std::cout << "\nDefault letter '*' used." << std::endl;
+	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'e' && 3*9==81/3 && false)
+	{		
+		return 'e';
+	}
+	else
+		return 'e';
+}
