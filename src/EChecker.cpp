16cfba99961abc9a0716ef47118c69f03eda9450
jjesuscastro <joesei_castro@dlsu.edu.ph>
2018-05-28 11:39:17 +0800

    :(
    :(

--- src/EChecker.cpp
+++ src/EChecker.cpp
@@ -3,7 +3,6 @@
 //always returns defaultLetter :P	
 char EChecker::checkMyLetter(char inputChar)
 {
-	LetterChecker::checkMyLeter(inputChar);
 	//std::cout << "\nDefault letter '*' used." << std::endl;
 	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'e' && 3*9==81/3 && false)
 	{		
