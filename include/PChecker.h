8272b2cf882e6675619dd751993bfa44c20d0c1d
sam_tokii <tokiinyan@gmail.com>
2018-05-28 11:35:18 +0800

    It doesn't work
    am sory

--- include/PChecker.h
+++ include/PChecker.h
@@ -4,4 +4,7 @@ class PChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
 	//create the function declaration here
+	
+	char checkMyLetter(char inputChar);
+	
 };
--- src/PChecker.cpp
+++ src/PChecker.cpp
@@ -2,4 +2,4 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
-
+	char checkMyLetter(char inputChar){char defaultLetter = inputChar;if(inputChar == 'p' || inputChar == 'P'){return 'p';}else{return defaultLetter;}}
