8bbb3c6b5860eab3b2e26950180e19abdece7342
ArlanGmz <34024601+ArlanGmz@users.noreply.github.com>
2018-05-28 11:43:21 +0800

    added stuff

--- src/KChecker.cpp
+++ src/KChecker.cpp
@@ -1,5 +1,13 @@
 #include "KChecker.h"
 
+char KChecker::checkMyLetter(char inputChar) 
+{
+
+		return 'k';
+}
+
+//sets defaultLetter to '*'
+
 //override the function "checkMyLetter"
 //create the function definition here
 
