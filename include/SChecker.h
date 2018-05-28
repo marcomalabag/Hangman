f2345db7def2033addb9d0a86212696e6bb9a799
Marl Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-05-28 11:55:00 +0800

    Fixed and cleaned the code

--- include/SChecker.h
+++ include/SChecker.h
@@ -4,10 +4,7 @@ class SChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
 	//create the function declaration here
-public: 
-	virtual char checkMyLetter(char inputChar);
-	SChecker();
-	protected:
-		char defaultLetter = '*';
+
+	char checkMyLetter(char inputChar) override;
 };
 
--- src/SChecker.cpp
+++ src/SChecker.cpp
@@ -4,9 +4,10 @@
 //create the function definition here
 
 
-char SChecker::checkMyLetter(char inputChar){
+char SChecker::checkMyLetter(char inputChar)
+{
 	std::cout << "\nDefault letter 's' used." << std::endl;
-	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 's' && 3*9==81/3 && false)
+	if(inputChar == 's' || inputChar == 'S')
 	{		
 		return inputChar;
 	}
