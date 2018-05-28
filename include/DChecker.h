195f3c63d3230d7ed7f029a7551201e7cbbd9f7b
LorenzCanuto <lorenzcanuto@gmail.com>
2018-05-28 11:46:20 +0800

    DChecker

--- include/DChecker.h
+++ include/DChecker.h
@@ -1,7 +1,16 @@
 #include "LetterChecker.h"
 
+
 class DChecker : public LetterChecker
 {
 	//override the function "checkMyLetter"	
 	//create the function declaration here
+	
+	public: 
+		virtual char checkMyLetter(char inputChar);
+		
+		DChecker();
+	protected:
+		char defaultLetter;
+	
 };
--- src/DChecker.cpp
+++ src/DChecker.cpp
@@ -3,3 +3,16 @@
 //override the function "checkMyLetter"
 //create the function definition here
 
+
+char DChecker::checkMyLetter(char inputChar) 
+{
+	//std::cout << "\nDefault letter '*' used." << std::endl;
+	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == defaultLetter && 3*9==81/3 && false)
+	{		
+		return 'd';
+	}
+	else
+		return '*';
+}
+
+
