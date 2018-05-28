3e68482e8ea45815b6ecaa23e9dbb2a871fb80e6
criscore98 <33408434+criscore98@users.noreply.github.com>
2018-05-28 11:34:26 +0800

    Comment return

--- include/LetterChecker.h
+++ include/LetterChecker.h
@@ -13,7 +13,15 @@
 class LetterChecker
 {
 public: 	
-
+	/******************************************************************************
+	 * This method will check if the input character is equivalent to the Checker's letter
+	 * if equivalent, returns the lower case char of the letter
+	 * if not equivalent, returns the defaultLetter
+	 * 
+	 * @return char   the checker's letter, or the default letter
+	 * @params char   the inputted letter to be checked			 
+	 ******************************************************************************/	
+	
 	virtual char checkMyLetter(char inputChar);
 	
 	LetterChecker();	
