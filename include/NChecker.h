2119376ce619573512c9afd38a2d2ecbd18419bf
Khobaka <khobertyves@gmail.com>
2018-05-28 11:41:10 +0800

    N checking
    codes to check if N is the input

--- include/NChecker.h
+++ include/NChecker.h
@@ -1,7 +1,22 @@
 #include "LetterChecker.h"
 
+#define __LETTERCHECKER_H_INCLUDED__
+
+#include <iostream>
 class NChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
 	//create the function declaration here
+public: 	
+	/******************************************************************************
+	 * This method will check if the input character is equivalent to the Checker's letter
+	 * if equivalent, returns the lower case char of the letter
+	 * if not equivalent, returns the defaultLetter
+	 * 
+	 * @return char   the checker's letter, or the default letter
+	 * @params char   the inputted letter to be checked			 
+	 ******************************************************************************/	
+	virtual char nchecker(char inputChar);
+	NChecker();
+
 };
--- src/NChecker.cpp
+++ src/NChecker.cpp
@@ -1,5 +1,18 @@
 #include "NChecker.h"
-
+#include <iostream>
+using namespace std;
 //override the function "checkMyLetter"
 //create the function definition here
+ 
+ char NChecker::nchecker(char inputChar){
+ 
+ if (inputChar == 'N' || inputChar == 'n')
+ {
+     return 'N';
+ }
+ else
+   return '*';
+}
+
+//sets defaultLetter to '*'
 
