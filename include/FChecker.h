572cde948b4fc131d32adaf38728aa7f13238d78
carlacresencio <carla_cresencio@dlsu.edu.ph>
2018-05-28 11:50:56 +0800

    It compiles at least
    but it dont printf and im angery

--- include/FChecker.h
+++ include/FChecker.h
@@ -4,4 +4,6 @@ class FChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
 	//create the function declaration here
+	public: 	
+		char checkMyLetter(char inputChar);
 };
--- src/FChecker.cpp
+++ src/FChecker.cpp
@@ -1,5 +1,13 @@
 #include "FChecker.h"
 
 //override the function "checkMyLetter"
+char FChecker::checkMyLetter(char inputChar) 
+{
+	char letter; 
+	letter = 'f';
+	printf("haha fuk");
+	return letter;
+}
+
 //create the function definition here
 
