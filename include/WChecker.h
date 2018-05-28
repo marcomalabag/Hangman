051ad6fa429a8ad441de0a49bdaa0fa18d203289
ajctan <33886537+ajctan@users.noreply.github.com>
2018-05-28 11:42:37 +0800

    Revert "accidentally did V instead of W (╯°□°）╯︵ ┻━┻"
    This reverts commit 292520a2a127313c262ae978cd87903e8d14f82e.

--- include/WChecker.h
+++ include/WChecker.h
@@ -2,7 +2,6 @@
 
 class WChecker : public LetterChecker
 {	
-	char checkMyLetter(char inputChar);
 	//override the function "checkMyLetter"	
 	//create the function declaration here
 };
--- src/WChecker.cpp
+++ src/WChecker.cpp
@@ -1,7 +1,5 @@
 #include "WChecker.h"
 
-char WChecker::checkMyLetter(char inputChar){if(inputChar == 'W' || inputChar == 'w')return 'w';else return defaultLetter;}//override the function "checkMyLetter"//create the function definition here
-
 //override the function "checkMyLetter"
 //create the function definition here
 
