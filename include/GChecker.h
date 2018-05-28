88d5434d7ead0c832c1a042a5bca495851832389
EGCruz <edmund_cruz@dlsu.edu.ph>
2018-05-28 11:41:22 +0800

    GChecker UPDATE

--- include/GChecker.h
+++ include/GChecker.h
@@ -1,7 +1,10 @@
 #include "LetterChecker.h"
+#include <iostream>
 
 class GChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
+	public:
+		char checkMyLetter(char inputChar) override;
 	//create the function declaration here
 };
