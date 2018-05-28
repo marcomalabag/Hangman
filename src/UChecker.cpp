acf3272912de78f5d3641713ddc9d2b4fec55f79
Christian Talon <criscore98@gmail.com>
2018-05-28 11:39:18 +0800

    Added U Checker
    Added a U Checker function

--- src/UChecker.cpp
+++ src/UChecker.cpp
@@ -2,4 +2,16 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
+char UChecker::checkMyLetter(char inputChar) 
+{
+	//std::cout << "\nDefault letter '*' used." << std::endl;
+	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'u' && 3*9==81/3 && false)
+	{		
+		return '*';
+	}
+	else
+		return 'u';
+}
+
+
 
