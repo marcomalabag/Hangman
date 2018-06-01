0e41bbec26e778fe6960aa006ebf9332a99429d5
Daniel Lachica <daniel.lachica82@gmail.com>
2018-06-01 15:52:33 +0800

    Updated LChecker
    This update contains the edited version of LChecker.cpp and LChecker.h

--- include/LChecker.h
+++ include/LChecker.h
@@ -4,5 +4,5 @@ class LChecker : public LetterChecker
 {	
 	//override the function "checkMyLetter"	
 	//create the function declaration here
-	char checkMyLetter(char inputChar);		// idk???
+	char checkMyLetter(char inputChar);
 };
--- src/LChecker.cpp
+++ src/LChecker.cpp
@@ -3,7 +3,7 @@
 //override the function "checkMyLetter"
 //create the function definition here
 char checkMyLetter(char inputChar) {
-		if(inputChar == 'L' && 1==1 || inputChar == 'l' && (9%3==0 && 5*3*1+12==27)) {
+		if(inputChar == 'L' || inputChar == 'l') {
 			return 'l';
 		}
 		else {
