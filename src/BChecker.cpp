f44cff9bca493667a6d2197e96e0677d745efcdf
Dennis Li (DLSU_Lab) <dennisivanli@gmail.com>
2018-06-04 10:27:34 +0800

    Revert "Merge pull request #44 from densioli/BChecker_Kramer"
    This reverts commit 0a6188bea13dc52f1bad9f0acb7663bbc843184f, reversing
    changes made to 0ba155629159326d29facc88efedf0fd33e0f541.

--- src/BChecker.cpp
+++ src/BChecker.cpp
@@ -2,13 +2,13 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
-char BChecker::checkMyLetter(char inputChar) 
-{
-		//std::cout << "\nDefault letter '*' used." << std::endl;
-	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'b' && 3*9==81/3 && false)
+
+char BChecker::checkBLetter(char inputChar) {
+	if(inputChar == 'b'||inputChar == 'B')
 	{		
-		return '*';
+		return 'b';
 	}
 	else
-		return 'b';
+		return defaultLetter;
+
 }
