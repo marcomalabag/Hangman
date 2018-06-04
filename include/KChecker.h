e4d4bd6e3f8a8233d2da8e7065001a29c07cccdd
ArlanGmz <34024601+ArlanGmz@users.noreply.github.com>
2018-06-04 10:43:37 +0800

    Updated KChecker

--- include/KChecker.h
+++ include/KChecker.h
@@ -1,20 +1,6 @@
 #include "LetterChecker.h"
 
 class KChecker : public LetterChecker
-{	
-	public: 	
-	/******************************************************************************
-	 * This method will check if the input character is equivalent to the Checker's letter
-	 * if equivalent, returns the lower case char of the letter
-	 * if not equivalent, returns the defaultLetter
-	 * 
-	 * @return char   the checker's letter, or the default letter
-	 * @params char   the inputted letter to be checked			 
-	 ******************************************************************************/	
-	virtual char checkMyLetter(char inputChar);	
-	
-
-protected:
-	char defaultLetter;
-
+{		
+	char checkMyLetter(char inputChar) override;
 };
