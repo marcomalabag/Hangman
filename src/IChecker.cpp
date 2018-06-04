d8724e2be04693d1682a59e71432cf5e7c17307b
jerseydeguzman <jersey.adelei@gmail.com>
2018-06-04 10:57:15 +0800

    Nakalimutan ko mag push amp

--- src/IChecker.cpp
+++ src/IChecker.cpp
@@ -6,11 +6,10 @@
 char IChecker::checkMyLetter(char inputChar) 
 {
 	//std::cout << "\nDefault letter '*' used." << std::endl;
-	if(inputChar == 'i'){
-		if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == defaultLetter && 3*9==81/3 && false)
-			return '*';
+	if(inputChar == 'i'||inputChar=='I'){
+		return 'i';
 	}
 	else
-		return 'i';
+		return '*';
 }
 
