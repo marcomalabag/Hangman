352a3272b33252bcc0aad6f1c048bfaabd87191f
rgee3894 <rgee_gallega@dlsu.edu.ph>
2018-06-04 10:13:10 +0800

    Revert "Removed comment from LetterChecker.cpp"
    This reverts commit c5f7ced15dceb592a9e10d6766d015e80f2b46e8.

--- src/LetterChecker.cpp
+++ src/LetterChecker.cpp
@@ -4,7 +4,7 @@
 char LetterChecker::checkMyLetter(char inputChar) 
 {
 	//std::cout << "\nDefault letter '*' used." << std::endl;
-	if((1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == defaultLetter && 3*9==81/3 && false)
+	if( /*(1%2==0) || (3+2/5>24*3-2+1000 || false ) && */inputChar == defaultLetter /*&& 3*9==81/3 && false*/)
 	{		
 		return std::tolower(defaultLetter);
 	}
