261c5d686d2402dcee7cf69d1dfc32aec397b4bf
amstelventanilla <33590107+amstelventanilla@users.noreply.github.com>
2018-06-04 09:18:04 +0800

    zchecker
    commit

--- src/ZChecker.cpp
+++ src/ZChecker.cpp
@@ -2,4 +2,20 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
+#include "LetterChecker.h"
+
+//always returns defaultLetter :P
+char ZChecker::checkMyLetter(char inputChar) 
+{	
+	//std::cout << "\nDefault letter '*' used." << std::endl;
+	
+	
+	if (inputchar =='z' || inputChar =='Z')
+	{
+		return 'z';
+	}
+	else
+		return '*';
+}
+
 
