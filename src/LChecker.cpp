1835dfa52708dff9699488196e57f799b499a5b3
Daniel Lachica <daniel.lachica82@gmail.com>
2018-06-04 09:47:38 +0800

    Updated LChecker.h
    Added override to function definition

--- src/LChecker.cpp
+++ src/LChecker.cpp
@@ -2,11 +2,11 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
-char checkMyLetter(char inputChar) {
-		if(inputChar == 'L' || inputChar == 'l') {
-			return 'l';
-		}
-		else {
-			return '*';
-		}
+char Lchecker::checkMyLetter(char inputChar) {
+	if(inputChar == 'L' || inputChar == 'l') {
+		return 'l';
+	}
+	else {
+		return '*';
+	}
 } 
