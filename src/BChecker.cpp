56a4ee2ba78ca9aa4e9a4584ac75300b27413928
kevzter18 <kevzter18@gmail.com>
2018-06-04 10:02:05 +0800

    BChecker_Kramer

--- src/BChecker.cpp
+++ src/BChecker.cpp
@@ -3,3 +3,13 @@
 //override the function "checkMyLetter"
 //create the function definition here
 
+char BChecker::checkMyLetter(char inputChar) 
+{
+		//std::cout << "\nDefault letter '*' used." << std::endl;
+	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'b' && 3*9==81/3 && false)
+	{		
+		return '*';
+	}
+	else
+		return 'b';
+}
