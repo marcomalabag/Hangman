55c78887ad528a3afc2adabe37c513cf34789643
rigeljoseph <rigeljosephdc@gmail.com>
2018-06-04 09:57:51 +0800

    updated

--- src/HChecker.cpp
+++ src/HChecker.cpp
@@ -5,7 +5,7 @@
 char HChecker::checkMyLetter(char inputChar) 
 {
 	//std::cout << "\nDefault letter '*' used." << std::endl;
-	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == defaultLetter && 3*9==81/3 && false)
+	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'h' && 3*9==81/3 && false)
 	{		
 		return 'h';
 	}
