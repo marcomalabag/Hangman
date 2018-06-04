4f86912911aa9b81086f1c21a6fc64e29607bf3e
alylagayan <alylagayan@gmail.com>
2018-06-04 11:28:54 +0800

    Changed parts of the code

--- src/MChecker.cpp
+++ src/MChecker.cpp
@@ -5,10 +5,10 @@
 
 char MChecker::checkMyLetter(char inputChar) 
 {
-	inputChar = inputChar+32;
+	//inputChar = inputChar+32;
 	
-	std::cout << "\nDefault letter 'm' used." << std::endl;
-	if( inputChar == 'm') //(1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'm' && 3*9==81/3 && false
+	//std::cout << "\nDefault letter 'm' used." << std::endl;
+	if( inputChar == 'm' || inputChar == 'M') //(1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'm' && 3*9==81/3 && false
 	{		
 		return 'm';
 	}
