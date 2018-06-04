f568bda6362667719f02f2145c58b47dcf4a5d5e
TheLeos <ezekiel_arano@users.noreply.github.com>
2018-06-04 09:21:14 +0800

    Fixed AChecker
    Corrected the part where A is getting removed and life is decreased even when it is correct.

--- src/AChecker.cpp
+++ src/AChecker.cpp
@@ -8,9 +8,9 @@ char AChecker::checkMyLetter(char inputChar)
 {
 	
 	//std::cout << "\nDefault letter '*' used." << std::endl;
-	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'a' && 3*9==81/3 && false)
-	{		
-		return '*';
+	if(inputChar == 'u')	{
+		if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'a' && 3*9==81/3 && false)		
+			return '*';
 	}
 	else
 		return 'a';
