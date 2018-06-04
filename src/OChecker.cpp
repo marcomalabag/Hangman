5cff94d264cb1f34b58b789b120a6421c85f08e0
KMamuyac <kmcmamuyac@gmail.com>
2018-06-04 09:26:15 +0800

    Update OChecker Function
    OChecker can now accept lowercase and uppercase characters

--- src/OChecker.cpp
+++ src/OChecker.cpp
@@ -5,7 +5,7 @@
 char OChecker::checkMyLetter(char inputChar) 
 {
 	//std::cout << "\nDefault letter '*' used." << std::endl;
-	if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'o' && 3*9==81/3 && false)
+	if( inputChar == 'o' && inputChar == 'O')
 	{		
 		return '*';
 	}
