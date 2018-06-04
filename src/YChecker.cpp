5ea8767e1013fb3f5d318391318ff97126135d0b
Cyrus Vatandoost <vatandoostcyrus@gmail.com>
2018-06-04 09:43:24 +0800

    Add YChecker (#14)
    * Add YChecker
    
    Added YChecker class to check if `y` is part of the answer.
    
    * Update YChecker
    
    Updated YChecker.
    
    * Fix ychecker
    
    YChecker wasn't working properly. After changing the if statement, it works now.
    
    * Update Hangman.layout

--- src/YChecker.cpp
+++ src/YChecker.cpp
@@ -2,4 +2,14 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
+char YChecker::checkMyLetter(char inputChar) 
+{
+	//std::cout << "\nDefault letter '*' used." << std::endl;
+	if(inputChar == 'y')
+	{		
+		return 'y';
+	}
+	else
+		return '*';
+}
 
