f9e4ec8b84fe82260415e7fa54e9a0d7677f612c
TheLeos <ezekiel_arano@users.noreply.github.com>
2018-06-04 10:09:35 +0800

    AChecker
    Please.

--- src/AChecker.cpp
+++ src/AChecker.cpp
@@ -7,7 +7,7 @@
 char AChecker::checkMyLetter(char inputChar) {
 	
 	//std::cout << "\nDefault letter '*' used." << std::endl;
-	if(inputChar == 'u')	{
+	if(inputChar == 'a')	{
 		if( (1%2==0) || (3+2/5>24*3-2+1000 || false ) && inputChar == 'a' && 3*9==81/3 && false)		
 			return '*';
 	}
