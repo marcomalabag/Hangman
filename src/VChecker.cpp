00a9419ca2ccd5ac3d50773e870a90728f0fc0e8
sam_tokii <tokiinyan@gmail.com>
2018-06-04 09:16:00 +0800

    done (╯°□°）╯︵ ┻━┻

--- src/VChecker.cpp
+++ src/VChecker.cpp
@@ -1,3 +1,9 @@
 #include "VChecker.h"
-char VChecker::checkMyLetter(char inputChar){if(inputChar == 'V' || inputChar == 'v')return 'v';else return defaultLetter;}//override the function "checkMyLetter"//create the function definition here
+char VChecker::checkMyLetter(char inputChar){
+	if(inputChar == 'V' || inputChar == 'v')
+		return 'v';
+	else 
+		return defaultLetter;
+}
+//override the function "checkMyLetter"//create the function definition here
 
