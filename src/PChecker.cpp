fffce8c83bfceba6c737424f6ed9fbfce6a0f6d4
sam_tokii <tokiinyan@gmail.com>
2018-06-11 09:30:01 +0800

    fix

--- src/PChecker.cpp
+++ src/PChecker.cpp
@@ -2,4 +2,14 @@
 
 //override the function "checkMyLetter"
 //create the function definition here
-	char checkMyLetter(char inputChar){char defaultLetter = inputChar;if(inputChar == 'p' || inputChar == 'P'){return 'p';}else{return defaultLetter;}}
+
+char PChecker::checkMyLetter(char inputChar)
+{
+	if(inputChar == 'p' || inputChar == 'P')
+	{
+		return 'p';
+	}
+	else{
+		return '*';
+	}
+}
